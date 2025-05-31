31c8b5e4b1be292b6b789ec5198aabcd78cc50e8
sovi <sovialfina@gmail.com>
2025-05-31 12:02:45 +0700

    paling fix tanpa allert login

--- app/Http/Controllers/admin/DashboardAdminController.php
+++ app/Http/Controllers/admin/DashboardAdminController.php
@@ -9,6 +9,28 @@ use App\Models\Kriteria;
 
 class DashboardAdminController extends Controller
 {
+public function index()
+{
+    $jumlahUser =User::all()->count();
+    $totalPelamar = User::where('role', 'pelamar')->count();
+    $totaladmin = User::where('role', 'admin')->count();
+    $totalhrd = User::where('role', 'hrd')->count();
 
+    $pelamarTidakDiterima = User::where('role', 'pelamar')
+        ->whereHas('hasilSeleksi', function ($query) {
+            $query->where('status', 'tidak diterima');
+        })
+        ->count();
+
+    $pelamarDiterima = User::where('role', 'pelamar')
+        ->whereHas('hasilSeleksi', function ($query) {
+            $query->where('status', 'diterima');
+        })
+        ->count();
+
+    $kriterias = Kriteria::all();
+
+    return view('admin.dashboard.dashboard', compact('totalPelamar', 'pelamarTidakDiterima', 'pelamarDiterima', 'jumlahUser', 'kriterias', 'totaladmin', 'totalhrd'));
+}
 
 }
